fb53f21f8531b2921ba2aef40919a69db5c6adf7
kumancev <kumancev1@gmail.com>
2022-08-16 12:51:50 +0300

    add third project (quiz)

--- src/App.js
+++ src/App.js
@@ -1,29 +1,81 @@
-import React, { useState } from 'react';
 import './index.scss';
+import { useState } from 'react';
 
+const questions = [
+  {
+    title: 'React - это ... ?',
+    variants: ['библиотека', 'фреймворк', 'приложение'],
+    correct: 0,
+  },
+  {
+    title: 'Компонент - это ... ',
+    variants: ['приложение', 'часть приложения или страницы', 'то, что я не знаю что такое'],
+    correct: 1,
+  },
+  {
+    title: 'Что такое JSX?',
+    variants: [
+      'Это простой HTML',
+      'Это функция',
+      'Это тот же HTML, но с возможностью выполнять JS-код',
+    ],
+    correct: 2,
+  },
+];
 
-const Modal = ({ isOpen, setIsOpen, children }) => (
-  <div className={`overlay animated ${isOpen ? 'show' : ''}`}>
-      <div className="modal">
-        <svg onClick={() => setIsOpen(false)} height="200" viewBox="0 0 200 200" width="200">
-          <title />
-          <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
-        </svg>
-        {children}
+function Result({ correct }) {
+  return (
+    <div className="result">
+      <img src="https://cdn-icons-png.flaticon.com/512/2278/2278992.png" alt='congrats' />
+      <h2>Вы отгадали {correct} ответа из {questions.length}</h2>
+      <a href='/'>
+        <button>Попробовать снова</button>
+      </a>
     </div>
-  </div>
-)
+  );
+}
+
+function Game({ question, onClickVariant, step }) {
+  const percentage = Math.round(step / questions.length * 100)
+
+  return (
+    <>
+      <div className="progress">
+        <div style={{ width: `${percentage}%` }} className="progress__inner"></div>
+      </div>
+      <h1>{question.title}</h1>
+      <ul>
+        {
+          question.variants.map((text, index) => (
+            <li key={text} onClick={() => onClickVariant(index)} >{text}</li>
+        ))}
+      </ul>
+    </>
+  );
+}
 
 function App() {
-  const [isOpen, setIsOpen] = useState(false)
-  
-  console.log(isOpen)
+  const [step, setStep] = useState(0)
+  const [correct, setCorrect] = useState(0)
+  const question = questions[step]
+
+  const onClickVariant = (index) => {
+    setStep(step + 1)
+
+    if (index === question.correct) {
+      setCorrect(correct + 1)
+    }
+  }
+
   return (
     <div className="App">
-      <button onClick={() => setIsOpen(true)} className="open-modal-btn">✨ Open window</button>
-      <Modal isOpen={isOpen} setIsOpen={setIsOpen}>
-        <img src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif" alt='gif'/>
-      </Modal>
+      {
+        step !== questions.length ? (
+          <Game step={step} question={question} onClickVariant={onClickVariant} />
+        ) : (
+          <Result correct={correct} />
+        )
+      }
     </div>
   );
 }
